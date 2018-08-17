e61e6589faac372753006f21f0fd98def1f97773
Michael Farr <mfarr@sm-energy.com>
2018-08-17 04:35:34 -0500

    8/17 edits

--- Join_prodvols.R
+++ Join_prodvols.R
@@ -7,8 +7,6 @@ library(dplyr)
 #get a list of data.frames
 file.names<-ls()[sapply(ls(), function(x) class(get(x))) == 'data.frame']
 
-
-i <- 2
 for(i in 1:length(file.names)){
 
 tmp<- get(file.names[i])
@@ -22,5 +20,17 @@ rm(tmp)
 
 }
 
+JOIN <- right_join(prop %>% select(API, PROPNUM, LEASE, TYPE_CURVE_REGION, TYPE_CURVE_SUBREGION, LATITUDE ,LATITUDE_BH, 
+                                   LONGITUDE, LONGITUDE_BH, FIRST_PROD, EFF_LAT, RSV_CAT), 
+                   daily %>% select(Aries_Prop_Num, Prod_Dt, Vol_Gas_Wh_Mcf, Vol_Gas_Prod_Mcf, Vol_Oil_Wh_Bbl, Vol_Oil_Prod_Bbl, Vol_Wtr_Wh_Bbl), 
+                   by = c("PROPNUM" = "Aries_Prop_Num"))
 
+
+
+
+
+
+TimeStamp=paste(date(),Sys.timezone())
+tdir = 'C:/Users/MFARR/Documents/R_files/Spotfire.data' # place to store diagnostics if it exists (otherwise do nothing)
+if(file.exists(tdir) && file.info(tdir)$isdir) suppressWarnings(try(save(list=ls(), file=paste(tdir,'/prodvol_tbls.RData',sep=''), RFormat=T )))
 
---
+++ prod_month.R
@@ -0,0 +1,45 @@
+rm(list = ls())
+
+load("C:/Users/mfarr/Documents/R_files/Spotfire.data/prodvol_month.RData")
+load("C:/Users/mfarr/Documents/R_files/Spotfire.data/prodvol_month_join.RData")
+
+
+library(dplyr)
+
+#get a list of data.frames
+file.names<-ls()[sapply(ls(), function(x) class(get(x))) == 'data.frame']
+
+for(i in 1:length(file.names)){
+  
+  tmp<- get(file.names[i])
+  names(tmp)<-gsub("-","", names(tmp))
+  names(tmp)<-gsub(" ", "_", names(tmp))
+  names(tmp)<-gsub("[()]", "", names(tmp))
+  names(tmp)<-gsub("/","_",  names(tmp))
+  names(tmp)<-gsub("\\%", "", names(tmp))
+  assign(file.names[i], tmp)
+  rm(tmp)
+  
+}
+
+
+JOIN <- right_join(prop %>% select(API, PROPNUM, LEASE, TYPE_CURVE_REGION, TYPE_CURVE_SUBREGION, LATITUDE ,LATITUDE_BH, 
+                                  LONGITUDE, LONGITUDE_BH, FIRST_PROD, EFF_LAT, RSV_CAT), 
+                   mon_prod %>% select(Aries_Prop_Num, Prod_Dt, Vol_Gas_Prod_Mcf, Vol_Oil_Prod_Bbl), by = c("PROPNUM" = "Aries_Prop_Num")) %>%
+  na.omit() %>%
+  group_by(PROPNUM) %>%
+  arrange(PROPNUM, Prod_Dt) %>%
+  mutate(VINTAGE = format(as.Date(FIRST_PROD, format = "%Y/%m/%d"), "%Y"), 
+         prod = 1, 
+         MONTHS = cumsum(prod),
+         CUMGAS_MCF = cumsum(Vol_Gas_Prod_Mcf), 
+         CUMOIL_BBL = cumsum(Vol_Oil_Prod_Bbl),
+         CUMGAS_3MMCF =  mean(ifelse(MONTHS == 3, CUMGAS_MCF / 1000, NA), na.rm = TRUE),
+         CUMGAS_6MMCF =  mean(ifelse(MONTHS == 6, CUMGAS_MCF / 1000, NA), na.rm = TRUE),
+         CUMGAS_12MMCF =  mean(ifelse(MONTHS == 12,CUMGAS_MCF / 1000, NA), na.rm = TRUE), 
+         CUMOIL_3MBO =  mean(ifelse(MONTHS == 3, CUMOIL_BBL / 1000, NA), na.rm = TRUE),
+         CUMOIL_6MBO =  mean(ifelse(MONTHS == 6, CUMOIL_BBL / 1000, NA), na.rm = TRUE),
+         CUMOIL_12MBO =  mean(ifelse(MONTHS == 12, CUMOIL_BBL / 1000, NA), na.rm = TRUE), 
+         VINTAGE = format(as.Date(FIRST_PROD, format = "%Y/%m/%d"), "%Y"))
+         
+
