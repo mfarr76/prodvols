b50e5e1268a2af023bbe8e39aabe9b199a0ed509
mfarr76 <mfarr76@gmail.com>
2018-08-22 17:04:25 -0500

    laptop update

--- Join_prodvols.R
+++ Join_prodvols.R
@@ -25,14 +25,43 @@ rm(tmp)
 
 }
 
-JOIN <- right_join(prop %>% select(API, PROPNUM, WELLCOMPID, LEASE, TYPE_CURVE_REGION, 
+JOIN <- right_join(prop %>% select(API, PROPNUM, WELLCOMPID, LEASE, BUDGET_NODE, TYPE_CURVE_REGION, 
                                    TYPE_CURVE_SUBREGION, LATITUDE ,LATITUDE_BH, LONGITUDE, 
                                    LONGITUDE_BH, FIRST_PROD, EFF_LAT, RSV_CAT), 
                    daily %>% select(Aries_Prop_Num, Prod_Dt, Vol_Gas_Wh_Mcf, Vol_Gas_Prod_Mcf, 
                                     Vol_Oil_Wh_Bbl, Vol_Oil_Prod_Bbl, Vol_Wtr_Wh_Bbl) %>%
-                     mutate(Prod_Dt = as.Date(Prod_Dt, "%Y/%m/%d"), 
-                            MON_END_DATE = ceiling_date(Prod_Dt, "month") - days(1)), 
-                   by = c("PROPNUM" = "Aries_Prop_Num")) 
+                     mutate(MON_END_DATE = ceiling_date(Prod_Dt, "month") - days(1)), 
+                   by = c("PROPNUM" = "Aries_Prop_Num")) %>%
+  group_by(PROPNUM) %>%
+  arrange(PROPNUM, Prod_Dt) %>%
+  mutate(prod = 1, 
+         DAYS = cumsum(prod), 
+         BOE = Vol_Oil_Wh_Bbl + Vol_Gas_Wh_Mcf / 6, 
+         CUMGAS_MCF = cumsum(Vol_Gas_Prod_Mcf), 
+         CUMOIL_BBL = cumsum(Vol_Oil_Prod_Bbl)) %>%
+  left_join(., gas %>% 
+  mutate(DATE = (ceiling_date(SampleDate, "month") - days(1))) %>%
+  group_by(WellCompletionEKey, DATE) %>%
+  select(DATE, WellCompletionEKey, Ethane_C2_Dry, Propane_C3_Dry, 
+         Isobutane_iC4_Dry, n_Butane_nC4_Dry,  Isopentane_iC5_Dry, 
+         n_Pentane_nC5_Dry, Hexanes_Plus_C6Plus_Dry, Heating_Value_Dry) %>%
+  mutate(GPM = Ethane_C2_Dry + Propane_C3_Dry + Isobutane_iC4_Dry +
+           n_Butane_nC4_Dry + Isopentane_iC5_Dry + n_Pentane_nC5_Dry + 
+           Hexanes_Plus_C6Plus_Dry, 
+         NGLYield = GPM / 42 * 1000) %>%
+  summarise(BTU = mean(Heating_Value_Dry, na.rm = TRUE),
+            C2 = mean(Ethane_C2_Dry, na.rm = TRUE),
+            C3 = mean(Propane_C3_Dry, na.rm = TRUE),
+            IC4 = mean(Isobutane_iC4_Dry, na.rm = TRUE),
+            NC4 = mean(n_Butane_nC4_Dry, na.rm = TRUE),
+            IC5 = mean(Isopentane_iC5_Dry, na.rm = TRUE),
+            NC5 = mean(n_Pentane_nC5_Dry, na.rm = TRUE),
+            C6Plus = mean(Hexanes_Plus_C6Plus_Dry, na.rm = TRUE),
+            GPM = mean(GPM, na.rm = TRUE), 
+            NGLYield = mean(NGLYield, na.rm = TRUE)), 
+  by = c("WELLCOMPID" = "WellCompletionEKey", c("MON_END_DATE" = "DATE"))) %>%
+  mutate()
+  
 
 
 dt <- gas %>% 
@@ -55,8 +84,6 @@ dt <- gas %>%
             C6Plus = mean(Hexanes_Plus_C6Plus_Dry, na.rm = TRUE),
             GPM = mean(GPM, na.rm = TRUE), 
             NGLYield = mean(NGLYield, na.rm = TRUE))
-
-
 
 
 
